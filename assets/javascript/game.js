ed5357ee0d3c3bc777f9b55a36fc97b8bffb71fe
Joseph Holder <josephholder@Josephs-MacBook-Pro.local>
2018-02-26 23:30:36 -0500

    1.2 JS - Most of functionality

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -4,8 +4,11 @@ var number = 101 + Math.floor(Math.random() * 120);
 //display random number
 $("#number").html(number);
 
-//set score to 0 initally
+//set globals
 var score = 0;
+var newScore = 0;
+var wins = 0;
+var losses = 0;
 
 //display score 
 $("#score").html(score);
@@ -16,10 +19,89 @@ var blue = 1 + Math.floor(Math.random() * 12);
 var yellow = 1 + Math.floor(Math.random() * 12);
 var green = 1 + Math.floor(Math.random() * 12);
 
+//rest game
+function reset() {
 
-//add red crystal value and score
-$( "#red" ).click(function() {
-    var newScore = score + red;
-    $("#score").html(newscore);
+    //reset game vars
+    var score = 0;
+    var newScore = 0;
+    var red = 1 + Math.floor(Math.random() * 12);
+    var blue = 1 + Math.floor(Math.random() * 12);
+    var yellow = 1 + Math.floor(Math.random() * 12);
+    var green = 1 + Math.floor(Math.random() * 12);
+}
 
-});
+//game function
+function runGame() {
+
+    //display score 
+    $("#score").html(score);
+
+    //display random number
+    $("#number").html(number);
+
+    //display win / losses
+    $("#wins").html(wins);
+    $("#losses").html(losses);
+
+    if (score < number) {
+
+        //add red crystal value and score
+        $("#red").click(function () {
+            newScore += red;
+
+            //update score with new value
+            $("#score").html(newScore);
+        });
+
+        //add blue crystal value and score
+        $("#blue").click(function () {
+            newScore += blue;
+
+            //update score with new value
+            $("#score").html(newScore);
+        });
+
+        //add yellow crystal value and score
+        $("#yellow").click(function () {
+            newScore += yellow;
+
+            //update score with new value
+            $("#score").html(newScore);
+        });
+
+        //add green crystal value and score
+        $("#green").click(function () {
+            newScore += green;
+
+            //update score with new value
+            $("#score").html(newScore);
+        });
+    }
+
+    //winner
+    else if (newScore === number) {
+
+        //update wins
+        wins++;
+
+        //update wins in html
+        $("#wins").html(wins);
+
+        //reset game
+    }
+
+    // looser
+    else{
+
+        //update losses
+        losses++;
+
+        //update losses in html
+        $("losses").html(losses);
+
+        //reset game
+
+    }
+}
+runGame();
